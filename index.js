485e2fa6612daf960f9938fc7c1ff8fbf8f32ed1
Oleksii Miroshko <miroshko@live.com>
2015-05-14 23:25:37 +0200

    RaceTrack

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
 	game_constructor: __dirname + '/race',
-	public_dir: __dirname + '/public',
-//	game_server_constructor: null, 	// default
-	io_exposed_methods: ['turn_intended', 'player_connected', 'start_game', 'create_game']	
+	public_dir: __dirname + '/public'
 }
