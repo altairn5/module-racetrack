e76ec209c87d5e8723c435c95f13237e5e326de5
Oleksii Miroshko <miroshko@live.com>
2015-05-16 00:06:15 +0200

    npm; tests and functionality for further turns

--- race.js
+++ race.js
@@ -20,7 +20,7 @@ Race.prototype._generateField = function(field) {
   return field;
 };
 
-Race.prototype._getCellsSquare = function(y1, x1, y2, x2) {
+Race.prototype._getCellsRect = function(y1, x1, y2, x2) {
   var ymin = Math.min(y1, y2);
   var ymax = Math.max(y1, y2);
   var xmin = Math.min(x1, x2);
@@ -53,10 +53,11 @@ Race.prototype._getRandomStartLine = function() {
 Race.prototype.start = function() {
   if (this.can_start) {
     var startLine = this._getRandomStartLine();
-    var possibleStartPositions = this._getCellsSquare(startLine.y1, startLine.x1, startLine.y2, startLine.x2);
+    var possibleStartPositions = this._getCellsRect(startLine.y1, startLine.x1, startLine.y2, startLine.x2);
     possibleStartPositions = possibleStartPositions.sort(function(a, b) { return Math.random() > 0.5 })
 
     for(var i = 0; i < this.players.length; i++) {
+      this.turns[this.players[i].id] = [];
       this.startPositions[this.players[i].id] = {
         y: possibleStartPositions[i].y,
         x: possibleStartPositions[i].x
@@ -73,13 +74,39 @@ Race.prototype.start = function() {
   }
 };
 
-Race.prototype.getPositions = function() {
-  return this.startPositions;
+Race.prototype._applyTurnVector = function(position, vector) {
+  return {y: position.y + vector.y, x: position.x + vector.x}
 };
 
+// @todo: add data redundancy for the sake of performance in case if the current approach with
+// each time calculation works bad
+Race.prototype.getPositionHistory = function(player) {
+  // @todo: replace with => once is supported by --harmony
+  var this_ = this;
+  if (!player)
+    return this.players.reduce(function(sum, player) {
+      sum[player.id] = this_.getPositionHistory(player);
+      return sum;
+    }, {});
+  
+  var positions = [this.startPositions[player.id]];
+
+  this.turns[player.id].forEach(function(turn) {
+    var last_position = positions[positions.length - 1];
+    positions.push(this._applyTurnVector(last_position, turn));
+  }, this);
+  
+  return positions;
+};
+
+
 Race.prototype.makePureTurn = function(player, turn) {
-  var bothZero = (Math.abs(turn.dx) || Math.abs(turn.dy)) == 0;
-  if (Math.abs(turn.dx) <= 1 && Math.abs(turn.dy) <= 1 && !bothZero) {
+  var lastTurn = this.turns[player.id][this.turns[player.id].length - 1] || {y: 0, x: 0};
+  var dx = Math.abs(lastTurn.x - turn.x);
+  var dy = Math.abs(lastTurn.y - turn.y);
+  var bothZero = dx == 0 && dy == 0;
+  
+  if (dx <= 1 && dy <= 1 && !bothZero) {
     if (!this.turns[player.id])
       this.turns[player.id] = [];
     this.turns[player.id].push(turn);
